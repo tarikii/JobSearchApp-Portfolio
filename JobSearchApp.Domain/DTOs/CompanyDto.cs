1e006792d37eb6302516ba50bfcce2ccc67a0327
Cath Cardenas <cathcardenasm@gmail.com>
2024-07-30 12:56:32 +0200

    CompanyDto created

---
+++ JobSearchApp.Domain/DTOs/CompanyDto.cs
@@ -0,0 +1,30 @@
+using JobSearchApp.Domain.Models;
+
+namespace JobSearchApp.Domain.DTOs
+{
+    public class CompanyDto
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public string WebsiteUrl { get; set; }
+        public string Industry { get; set; }
+        public string Size { get; set; }
+        public string Headquarters { get; set; }
+        public int FoundedYear { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+
+        public CompanyDto(Company company)
+        {
+            CompanyId = company.CompanyId;
+            Name = company.Name;
+            WebsiteUrl = company.WebsiteUrl;
+            Industry = company.Industry;
+            Size = company.Size;
+            Headquarters = company.Headquarters;
+            FoundedYear = company.FoundedYear;
+            Description = company.Description;
+            Location = company.Location;
+        }
+    }
+}
