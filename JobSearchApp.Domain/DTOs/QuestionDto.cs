23f6eaa9991d481907d9af50ca0b9532b79ae4c6
Cath Cardenas <cathcardenasm@gmail.com>
2024-07-30 12:58:02 +0200

    QuestionDto created

---
+++ JobSearchApp.Domain/DTOs/QuestionDto.cs
@@ -0,0 +1,16 @@
+using JobSearchApp.Domain.Models;
+
+namespace JobSearchApp.Domain.DTOs
+{
+    public class QuestionDto
+    {
+        public int QuestionId { get; set; }
+        public string QuestionText { get; set; }
+
+        public QuestionDto(Question question)
+        {
+            QuestionId = question.QuestionId;
+            QuestionText = question.QuestionText;
+        }
+    }
+}
