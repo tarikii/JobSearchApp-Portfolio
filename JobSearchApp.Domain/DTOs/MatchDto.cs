27d650696495462db91d38a11b7ec7865f3e92b3
Cath Cardenas <cathcardenasm@gmail.com>
2024-07-30 13:06:17 +0200

    MatchDto created

---
+++ JobSearchApp.Domain/DTOs/MatchDto.cs
@@ -0,0 +1,27 @@
+using JobSearchApp.Domain.Models;
+
+namespace JobSearchApp.Domain.DTOs
+{
+    public class MatchDto
+    {
+        public int MatchId { get; set; }
+        public int JobOfferId { get; set; }
+        public int UserId { get; set; }
+        public DateTimeOffset MatchDate { get; set; }
+        public bool IsAccepted { get; set; }
+
+        public string JobTitle { get; set; }
+        public string UserName { get; set; }
+
+        public MatchDto(Match match)
+        {
+            MatchId = match.MatchId;
+            JobOfferId = match.JobOfferId;
+            UserId = match.UserId;
+            MatchDate = match.MatchDate;
+            IsAccepted = match.IsAccepted;
+            JobTitle = match.JobOffer?.Title;
+            UserName = match.User?.UserName;
+        }
+    }
+}
