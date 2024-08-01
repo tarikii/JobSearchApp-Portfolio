92f5779de03bf233c18d627f4792f5f618967667
Cath Cardenas <cathcardenasm@gmail.com>
2024-08-01 16:09:01 +0200

    IWorkExperienceService created

--- JobSearchApp.BusinessLogic/Interfaces/IUserPreferenceService.cs
+++ JobSearchApp.BusinessLogic/Interfaces/IUserPreferenceService.cs
@@ -1,11 +1,12 @@
 using JobSearchApp.BusinessLogic.DTOs;
 
 namespace JobSearchApp.BusinessLogic.Interfaces;
-    public interface IUserPreferenceService
-    {
-        Task<IEnumerable<UserPreferenceDto>> GetAllUserPreferencesAsync();
-        Task<UserPreferenceDto> GetUserPreferenceByIdAsync(int id);
-        Task<UserPreferenceDto> CreateUserPreferenceAsync(UserPreferenceDto userPreferenceDto);
-        Task<bool> UpdateUserPreferenceAsync(UserPreferenceDto userPreferenceDto);
-        Task<bool> DeleteUserPreferenceAsync(int id);
-    }
+
+public interface IUserPreferenceService
+{
+    Task<IEnumerable<UserPreferenceDto>> GetAllUserPreferencesAsync();
+    Task<UserPreferenceDto> GetUserPreferenceByIdAsync(int id);
+    Task<UserPreferenceDto> CreateUserPreferenceAsync(UserPreferenceDto userPreferenceDto);
+    Task<bool> UpdateUserPreferenceAsync(UserPreferenceDto userPreferenceDto);
+    Task<bool> DeleteUserPreferenceAsync(int id);
+}
