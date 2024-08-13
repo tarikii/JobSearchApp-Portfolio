44f36d2f85cd78cd3cdf7e47c112373d3b0169d6
tarikii <tarikaabouchazougarh@gmail.com>
2024-08-13 13:30:22 +0200

    information-view

--- JobSearchApp.Views/wwwroot/js/user/login.js
+++ JobSearchApp.Views/wwwroot/js/user/login.js
@@ -1,22 +1,28 @@
-﻿document.getElementById('loginBtn').addEventListener('click', function () {
-
+﻿document.getElementById('loginBtn').addEventListener('click', function (event) {
     // Capturar los valores de los campos de entrada
-    const username = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    var form = document.querySelector(".validate-form");
 
-    // Crear un objeto con los datos que se enviarán
-    const data = {
-        username: username,
-        password: password
-    };
+    if (isValidForm(form, event)) {
+        const username = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
 
-    // Realizar la solicitud POST con Axios
-    axios.post('https://localhost:7056/api/User/authenticate', data)
-        .then(function (response) {
-            window.location.href = "/pages/home/HomeUserCandidate.html";
-        })
-        .catch(function (error) {
-            // Manejar los errores
-            console.error('Error en la solicitud:', error);
-        });
+        // Crear un objeto con los datos que se enviarán
+        const data = {
+            username: username,
+            password: password
+        };
+
+        // Realizar la solicitud POST con Axios
+        axios.post('https://localhost:7056/api/User/authenticate', data)
+            .then(function (response) {
+                console.log(response.data);
+                //window.location.href = "/pages/HomeUserBusinessReclutator.html";
+            })
+            .catch(function (error) {
+                // Manejar los errores
+                console.error('Error en la solicitud:', error.response.data);
+            });
+    } else {
+        event.preventDefault();
+    }
 });
